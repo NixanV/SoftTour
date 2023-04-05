bc65a4b6dda137869b4355116bf9c7af3522c5b0
Nikolay Petrov <nikip1303@gmail.com>
2023-04-05 11:58:50 +0300

    delete and edit buttons added

--- client/src/components/TourDetails/TourDetail.js
+++ client/src/components/TourDetails/TourDetail.js
@@ -1,13 +1,15 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import * as tourService from "../../services/tourService";
 import styles from "./styles/details.module.css";
+import { AuthContext } from "../../contexts/authContext";
 
 
 export const TourDetail = () => {
   //const [author, setAuthor] = useState('');
     const { tourId } = useParams();
     const [tour, setTour] = useState({});
+    const {user} = useContext(AuthContext);
 
     const navigate = useNavigate();
 
@@ -27,6 +29,14 @@ export const TourDetail = () => {
             <h2 className={styles["heading"]}>Tour in {tour.destination}</h2>
             <p className={styles["paragraph-description"]}>{tour.description}</p>
             <h4 className={styles["author"]}>{tour.author}</h4>
+            {user.accessToken ? 
+            <button className={styles["edit-btn"]}><i className="fas fa-edit" />Edit</button> : 
+            null}
+
+            {user.accessToken ? 
+            <button className={styles["delete-btn"]}><i className="fas fa-trash-alt" />Delete</button> : 
+            null}
+
             <button className={styles["back-button"]} onClick={() => navigate(-1)}>
             <i className="fas fa-chevron-circle-left" />
             Back
