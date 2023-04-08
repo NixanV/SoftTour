a81913b39d46be0b285adacbb9031bfb45f368f2
Nikolay Petrov <nikip1303@gmail.com>
2023-04-08 15:18:16 +0300

    styles edit

--- client/src/components/Profile/UserTourItem.js
+++ client/src/components/Profile/UserTourItem.js
@@ -1,13 +1,15 @@
 import styles from './styles/profile.module.css'
-
+import { Link } from 'react-router-dom'
 export const UserTourItem = ({
+    _id,
     destination,
     imageUrl
 }) => {
     return (
         <div className={styles["tour-item-profile"]}>
             <img className={styles["image-tour-profile"]}  src={imageUrl} alt="" />
-            <h2 className={styles["profile-destination"]}>{destination}</h2>
+            <Link className={styles["link-to-details"]} to={`/tours/${_id}`}>{destination}</Link>
+            
         </div>
     )
 }
